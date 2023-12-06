9a9d1baa84c471c7ce654967cea0a3d8f490e37b
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2023-12-06 17:53:21 +0400

    server actions and server components

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from "@/db";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import * as actions from"@/app/actions"
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 2000));
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: +props.params.id,
+    },
+  });
+  if (!snippet) {
+    notFound();
+  }
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null,snippet.id);
+
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            className="p-2 border rounded"
+            href={`/snippets/${props.params.id}/edit`}
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
+}
